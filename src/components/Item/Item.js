2f14655787aebd42739409bbc9a799ce6ed05e20
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-05-09 14:47:41 +0600

    style add to  items

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
-
+import './Item.css';
 const Item = (props) => {
     const { img, name, description } = props.item;
     return (
-        <div className='container'>
+        <div className='item'>
             <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={img} />
+                <Card.Img variant="top"style={{height:'250px'}} src={img} />
                 <Card.Body>
                     <Card.Title>{name}</Card.Title>
                     <Card.Text>
--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import Item from '../Item/Item';
-
+import './Items.css';
 const Items = () => {
-    const [items,setItems] = useState([]);
-    useEffect(()=>{
-        fetch('data.json').then(res=>res.json()).then(data=>setItems(data))
-    },[])
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        fetch('data.json').then(res => res.json()).then(data => setItems(data))
+    }, [])
     return (
-        <div>
-            <h1>demo</h1>
-            {
-                items.map(item=><Item
-                key={item.id}
-                item={item}
-                ></Item>)
-            }
+        <div className='container mt-5'>
+            <h1>Six Items Demos Available Laptop</h1>
+            <div className="items mt-5">
+                {
+                    items.map(item => <Item
+                        key={item.id}
+                        item={item}
+                    ></Item>)
+                }
+            </div>
         </div>
     );
 };
